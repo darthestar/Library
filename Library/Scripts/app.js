7a413e7285307e9e4822946a60004af8915ba27d
Sparks <dsparks@proforma.com>
2018-04-24 00:27:59 -0400

    search for book

--- Library/Scripts/app.js
+++ Library/Scripts/app.js
@@ -3,11 +3,43 @@
 angular
     .module("LibraryApp", [])
     .controller("MainAppController", ["$scope", "$http", ($scope, $http) => {
+        let booklist = [];
+        let _url = "/api/book";
         $http({
             method: "GET",
-            url: "/api/book"
+            url: _url,
         }).then(resp => {
-            console.log(resp.data);
             $scope.books = resp.data;
-        })
+            console.log($scope.books);
+            var titles = $scope.books.map((booktitle =>
+                booklist.push(booktitle["Title"].toLowerCase())
+            ));
+        });
+
+        $scope.checkForBook = () => {
+            console.log("looking for book")
+            var isBookinLibrary = booklist.indexOf($scope.userInquiry.toLowerCase());
+            console.log($scope.userInquiry.toLowerCase());
+            console.log(isBookinLibrary);
+            if (isBookinLibrary >-1) {
+                console.log("do you want to check it out?")
+                _url = "api/book/title=" + booklist[isBookinLibrary];
+                getBookToCheckout(_url);
+            }
+            else {
+                console.log("Book is not in our library")
+            }
+            
+        }
+
+        const getBookToCheckout =(url) => {
+            return $http({
+                method: "GET",
+                url: url,
+            }).then(response => {
+                console.log(response.data)
+                $scope.bookTitleToCheckout = response.data;
+            });
+        }
+    
     }]);
